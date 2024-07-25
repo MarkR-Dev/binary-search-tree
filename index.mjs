23a1cc497480bb07ac2dcd5f2cd02d5477013285
Mark Rodgers <markrodgers.dev@gmail.com>
2024-07-25 18:58:27 +0100

    Add method testing

--- index.mjs
+++ index.mjs
@@ -1,17 +1,54 @@
 import Tree from "./tree.mjs";
 
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+function randomArray() {
+  const arr = [];
+  const size = Math.floor(Math.random() * 50 + 1);
 
-const tree = new Tree(arr);
+  for (let i = 0; i < size; i++) {
+    arr.push(Math.floor(Math.random() * 100 + 1));
+  }
+
+  return arr;
+}
+
+const tree = new Tree(randomArray());
 
 tree.prettyPrint();
-tree.insert(56);
-tree.insert(57);
-tree.insert(-34);
-tree.insert(-56);
-tree.insert(560);
+
+console.log("\nIs balanced:", tree.isBalanced());
+
+const preorder = [];
+tree.preOrder((node) => preorder.push(node.data));
+
+const inorder = [];
+tree.inOrder((node) => inorder.push(node.data));
+
+const postorder = [];
+tree.postOrder((node) => postorder.push(node.data));
+
+console.log("Preoder:", preorder);
+console.log("Inoder:", inorder);
+console.log("Postoder:", postorder);
+
+for (let i = 0; i < 10; i++) {
+  tree.insert(Math.floor(Math.random() * 10 + 100));
+}
+
 tree.prettyPrint();
-console.log(tree.isBalanced());
+
+console.log("\nIs balanced:", tree.isBalanced());
+console.log("Rebalancing...");
 tree.rebalance();
 tree.prettyPrint();
-console.log(tree.isBalanced());
+console.log("\nIs balanced:", tree.isBalanced());
+
+preorder.length = 0;
+inorder.length = 0;
+postorder.length = 0;
+tree.preOrder((node) => preorder.push(node.data));
+tree.inOrder((node) => inorder.push(node.data));
+tree.postOrder((node) => postorder.push(node.data));
+
+console.log("\nPreoder:", preorder);
+console.log("Inoder:", inorder);
+console.log("Postoder:", postorder);
